465e593c4ece1eb83a91c6ea24869f7b10f134ed
Yashith Chanuka <chanu@Yashiths-MacBook-Air.local>
2023-03-11 15:09:06 +0530

    tailwindcss configured

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'accent': {
+          'navy': {
+            200: '#1A2942',
+            100: '#485468',
+            75: '#bfd2f2',
+            50: '#d7e3f7'
+          }
+        }  
+      }
+    },
+    
+  },
+  plugins: [],
+}
